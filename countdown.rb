f6bd5b9bb9a4fe9796b956728f60ca2f103c2933
Allison Nolan <allisonknolan@gmail.com>
2020-11-17 14:39:25 -0600

    commit tree 0771814b91627403bc950d050b59edf7ccea8421

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+    seconds = 10
+    while seconds > 0
+        seconds -= 1
+        puts "#{seconds} SECONDS"
+    when seconds == 0
+        return "HAPPY NEW YEAR!"
+    end
 end
